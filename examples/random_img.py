96fe647a4cd75509da9a84110b27a4b6f2909a43
fbjorn <kopfabschneider@yandex.ru>
2015-11-21 22:39:24 +0300

    Minor fixes.

--- examples/random_img.py
+++ examples/random_img.py
@@ -6,13 +6,13 @@ import webbrowser
 from chan.api import Page
 
 # get images from nulch
-initial_posts = [thread.posts[0] for thread in Page('b', 0).threads]
+initial_posts = [thread.original_post for thread in Page('b', 0).threads]
 
 attachments = [initial_post.files[0].url
                for initial_post in initial_posts
                if initial_post.files[0].is_picture()]
 
-pic = random.choice([attachment for attachment in attachments])
+pic = random.choice(attachments)
 
 print 'roll is', pic
 try:
